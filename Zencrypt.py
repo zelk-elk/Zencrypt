4d5a9412d738f76fcab5e0d6d16efd2caad98f4f
zelk-elk <139708448+zelk-elk@users.noreply.github.com>
2023-07-19 10:54:05 +0100

    Update Zencrypt.py
    Improved encryption method while making sure it's compatible for all operating systems

--- Zencrypt.py
+++ Zencrypt.py
@@ -1,24 +1,38 @@
 import os
 import sys
 import getpass
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Protocol.KDF import PBKDF2
-from tqdm import tqdm
-from colorama import Fore, Style
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import hashes, padding
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from colorama import Fore, Style, init
+
+init()
 
 def derive_key(password, salt, key_length):
-    return PBKDF2(password, salt, dkLen=key_length)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=key_length,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return kdf.derive(password.encode())
 
 def encrypt_file(input_file, output_directory, password):
-    salt = get_random_bytes(16)
+    salt = os.urandom(16)
     key = derive_key(password, salt, 32)
-    cipher = AES.new(key, AES.MODE_EAX)
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
 
     with open(input_file, 'rb') as file:
         plaintext = file.read()
 
-    ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(plaintext) + padder.finalize()
+
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
 
     encrypted_filename = os.path.basename(input_file) + '.enc'
     os.makedirs(output_directory, exist_ok=True)
@@ -26,11 +40,8 @@ def encrypt_file(input_file, output_directory, password):
 
     with open(encrypted_file, 'wb') as file:
         file.write(salt)
-        file.write(cipher.nonce)
-        file.write(tag)
+        file.write(iv)
         file.write(ciphertext)
-
-    os.remove(input_file)  
 
     return True
 
@@ -38,13 +49,17 @@ def decrypt_file(input_file, output_directory, password):
     try:
         with open(input_file, 'rb') as file:
             salt = file.read(16)
-            nonce = file.read(16)
-            tag = file.read(16)
+            iv = file.read(16)
             ciphertext = file.read()
 
         key = derive_key(password, salt, 32)
-        cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
-        plaintext = cipher.decrypt_and_verify(ciphertext, tag)
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+
+        decryptor = cipher.decryptor()
+        padded_data = decryptor.update(ciphertext) + decryptor.finalize()
+
+        unpadder = padding.PKCS7(128).unpadder()
+        plaintext = unpadder.update(padded_data) + unpadder.finalize()
 
         decrypted_filename = os.path.splitext(os.path.basename(input_file))[0]
         os.makedirs(output_directory, exist_ok=True)
@@ -53,79 +68,83 @@ def decrypt_file(input_file, output_directory, password):
         with open(decrypted_file, 'wb') as file:
             file.write(plaintext)
 
-        os.remove(input_file)  
-
         return True
 
     except FileNotFoundError:
         return False
 
 def display_banner():
-    banner = r"""
-__________                                         __   
-\____    /____   ____   ___________ ___.__._______/  |_ 
-  /     // __ \ /    \_/ ___\_  __ <   |  |\____ \   __\
- /     /\  ___/|   |  \  \___|  | \/\___  ||  |_> >  |  
-/_______ \___  >___|  /\___  >__|   / ____||   __/|__|  
-        \/   \/     \/     \/       \/     |__|         
-    """
-    print(Fore.YELLOW + banner + Style.RESET_ALL)
+    banner = f"""{Fore.YELLOW}
+__________                                         __
+\____    /____   ____   ___________ ___.__._______/  |_
+  /     // __ \ /    \_/ ___\_  __ <   |  |\____ \   __\\
+ /     /\  ___/|   |  \  \___|  | \/\___  ||  |_> >  |
+/_______ \___  >___|  /\___  >__|   / ____||   __/|__|
+        \/   \/     \/     \/       \/     |__|
+{Style.RESET_ALL}"""
+    print(banner)
 
 def display_files_in_directory(directory):
     files = os.listdir(directory)
 
-    print("Files in the chosen directory:")
+    print(f"{Fore.YELLOW}Files in the chosen directory:{Style.RESET_ALL}")
     for i, file in enumerate(files):
-        print(f"{i+1}. {file}")
+        print(f"{Fore.CYAN}{i+1}. {file}{Style.RESET_ALL}")
 
 def get_user_input():
-    print("Select an option:")
-    print("1. Encrypt a file")
-    print("2. Decrypt a file")
-    option = input("Enter the option number: ")
-    if option not in ['1', '2']:
-        print("Invalid option. Please select 1 or 2.")
+    print(f"{Fore.YELLOW}Select an option:{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}1. Encrypt a file{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}2. Decrypt a file{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}3. Quit{Style.RESET_ALL}")
+    option = input(f"{Fore.YELLOW}Enter the option number: {Style.RESET_ALL}")
+    if option not in ['1', '2', '3']:
+        print(f"{Fore.RED}Invalid option. Please select 1, 2, or 3.{Style.RESET_ALL}")
         sys.exit(1)
 
-    directory = input("Enter the directory: ")
-    filename = input("Enter the filename: ")
-
-    return option, directory, filename
+    if option in ['1', '2']:
+        directory = input(f"{Fore.YELLOW}Enter the directory: {Style.RESET_ALL}")
+        filename = input(f"{Fore.YELLOW}Enter the filename: {Style.RESET_ALL}")
+        return option, directory, filename
+    else:
+        return option, None, None
 
 def main():
     display_banner()
-    option, directory, filename = get_user_input()
 
-    if directory == '-':
-        directory = os.getcwd()
+    while True:
+        option, directory, filename = get_user_input()
 
-    output_directory = directory
+        if option == '3':
+            print(f"{Fore.YELLOW}Exiting Zencrypt...{Style.RESET_ALL}")
+            sys.exit()
 
-    password = getpass.getpass("Enter the encryption/decryption password: ")
+        if option in ['1', '2']:
+            if directory == '-':
+                directory = os.getcwd()
 
-    display_files_in_directory(directory)
+            output_directory = directory
 
-    if option == '1':
-        input_file = os.path.join(directory, filename)
-        print(f"Encrypting file: {filename}")
-        with tqdm(total=os.path.getsize(input_file), unit='B', unit_scale=True) as progress:
-            if encrypt_file(input_file, output_directory, password):
-                os.remove(input_file)  
-                progress.update(os.path.getsize(input_file))
-                print("Encrypted Successfully")
-            else:
-                print("Failed to encrypt file. File not found.")
+            password = getpass.getpass(f"{Fore.YELLOW}Enter the encryption/decryption password: {Style.RESET_ALL}")
 
-    elif option == '2':
-        input_file = os.path.join(directory, filename)
-        print(f"Decrypting file: {filename}")
-        with tqdm(total=os.path.getsize(input_file), unit='B', unit_scale=True) as progress:
-            if decrypt_file(input_file, output_directory, password):
-                os.remove(input_file)  
-                progress.update(os.path.getsize(input_file))
-                print("Decrypted Successfully")
-            else:
-                print("Failed to decrypt file. File not found.")
+            if option == '1':
+                input_file = os.path.join(directory, filename)
+                print(f"{Fore.YELLOW}Encrypting file: {filename}{Style.RESET_ALL}")
+                if encrypt_file(input_file, output_directory, password):
+                    os.remove(input_file)
+                    print(f"{Fore.GREEN}Encrypted Successfully{Style.RESET_ALL}")
+                    print("=" * 30)
+                else:
+                    print(f"{Fore.RED}Failed to encrypt file. File not found.{Style.RESET_ALL}")
+
+            elif option == '2':
+                input_file = os.path.join(directory, filename)
+                print(f"{Fore.YELLOW}Decrypting file: {filename}{Style.RESET_ALL}")
+                if decrypt_file(input_file, output_directory, password):
+                    os.remove(input_file)
+                    print(f"{Fore.GREEN}Decrypted Successfully{Style.RESET_ALL}")
+                    print("=" * 30)
+                else:
+                    print(f"{Fore.RED}Failed to decrypt file. File not found.{Style.RESET_ALL}")
 
 if __name__ == "__main__":
     main()
