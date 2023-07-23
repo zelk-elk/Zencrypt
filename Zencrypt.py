a67d73adebed2c5164ffb0d9b847b76f69710139
zelk-elk <139708448+zelk-elk@users.noreply.github.com>
2023-07-23 16:24:32 +0100

    Update Zencrypt.py

--- Zencrypt.py
+++ Zencrypt.py
@@ -2,28 +2,41 @@ import os
 import sys
 import getpass
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import hashes, padding
+from cryptography.hazmat.primitives import padding, hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 from colorama import Fore, Style, init
 
 init()
 
-def derive_key(password, salt, key_length):
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=key_length,
-        salt=salt,
-        iterations=100000,
-        backend=default_backend()
-    )
-    return kdf.derive(password.encode())
+def derive_key(password, salt, key_length, algorithm):
+    if algorithm == 'AES':
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=key_length,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+    elif algorithm == 'TripleDES':
+        password = password.ljust(24, b'\x00')[:24]
+        return password
+    else:
+        raise ValueError("Invalid encryption algorithm")
 
-def encrypt_file(input_file, output_directory, password):
+    return kdf.derive(password)
+
+def encrypt_file(input_file, output_directory, password, algorithm):
     salt = os.urandom(16)
-    key = derive_key(password, salt, 32)
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    key = derive_key(password, salt, 32, algorithm)
+
+    if algorithm == 'TripleDES':
+        iv = os.urandom(8)
+    else:
+        iv = os.urandom(16)
+
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend()) if algorithm == 'AES' else Cipher(
+        algorithms.TripleDES(key), modes.CBC(iv), backend=default_backend())
 
     with open(input_file, 'rb') as file:
         plaintext = file.read()
@@ -45,21 +58,31 @@ def encrypt_file(input_file, output_directory, password):
 
     return True
 
-def decrypt_file(input_file, output_directory, password):
+def decrypt_file(input_file, output_directory, password, algorithm):
     try:
         with open(input_file, 'rb') as file:
             salt = file.read(16)
-            iv = file.read(16)
+
+            if algorithm == 'TripleDES':
+                iv = file.read(8)
+            else:
+                iv = file.read(16)
+
             ciphertext = file.read()
 
-        key = derive_key(password, salt, 32)
-        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        key = derive_key(password, salt, 32, algorithm)
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend()) if algorithm == 'AES' else Cipher(
+            algorithms.TripleDES(key), modes.CBC(iv), backend=default_backend())
 
         decryptor = cipher.decryptor()
         padded_data = decryptor.update(ciphertext) + decryptor.finalize()
 
         unpadder = padding.PKCS7(128).unpadder()
-        plaintext = unpadder.update(padded_data) + unpadder.finalize()
+        try:
+            plaintext = unpadder.update(padded_data) + unpadder.finalize()
+        except ValueError:
+            print(f"{Fore.RED}Failed to decrypt file. Wrong password.{Style.RESET_ALL}")
+            return False
 
         decrypted_filename = os.path.splitext(os.path.basename(input_file))[0]
         os.makedirs(output_directory, exist_ok=True)
@@ -74,15 +97,15 @@ def decrypt_file(input_file, output_directory, password):
         return False
 
 def display_banner():
-    banner = f"""{Fore.YELLOW}
+    lblaka = f"""{Fore.YELLOW}
 __________                                         __
 \____    /____   ____   ___________ ___.__._______/  |_
-  /     // __ \ /    \_/ ___\_  __ <   |  |\____ \   __\\
+  /     // __ \ /    \_/ ___\_  __ <   |  |\____ \   __/
  /     /\  ___/|   |  \  \___|  | \/\___  ||  |_> >  |
 /_______ \___  >___|  /\___  >__|   / ____||   __/|__|
-        \/   \/     \/     \/       \/     |__|
+        \/   \/     \/ DWG \/       \/     |__|
 {Style.RESET_ALL}"""
-    print(banner)
+    print(lblaka)
 
 def display_files_in_directory(directory):
     files = os.listdir(directory)
@@ -104,15 +127,16 @@ def get_user_input():
     if option in ['1', '2']:
         directory = input(f"{Fore.YELLOW}Enter the directory: {Style.RESET_ALL}")
         filename = input(f"{Fore.YELLOW}Enter the filename: {Style.RESET_ALL}")
-        return option, directory, filename
+        algorithm = input(f"{Fore.YELLOW}Choose the encryption algorithm (AES or TripleDES): {Style.RESET_ALL}")
+        return option, directory, filename, algorithm  # Return the chosen algorithm as well
     else:
-        return option, None, None
+        return option, None, None, None  # Return None for the algorithm in case of '3'
 
 def main():
     display_banner()
 
     while True:
-        option, directory, filename = get_user_input()
+        option, directory, filename, algorithm = get_user_input()
 
         if option == '3':
             print(f"{Fore.YELLOW}Exiting Zencrypt...{Style.RESET_ALL}")
@@ -129,7 +153,7 @@ def main():
             if option == '1':
                 input_file = os.path.join(directory, filename)
                 print(f"{Fore.YELLOW}Encrypting file: {filename}{Style.RESET_ALL}")
-                if encrypt_file(input_file, output_directory, password):
+                if encrypt_file(input_file, output_directory, password.encode(), algorithm):  # Convert password to bytes
                     os.remove(input_file)
                     print(f"{Fore.GREEN}Encrypted Successfully{Style.RESET_ALL}")
                     print("=" * 30)
@@ -139,12 +163,12 @@ def main():
             elif option == '2':
                 input_file = os.path.join(directory, filename)
                 print(f"{Fore.YELLOW}Decrypting file: {filename}{Style.RESET_ALL}")
-                if decrypt_file(input_file, output_directory, password):
+                if decrypt_file(input_file, output_directory, password.encode(), algorithm):  # Convert password to bytes
                     os.remove(input_file)
                     print(f"{Fore.GREEN}Decrypted Successfully{Style.RESET_ALL}")
                     print("=" * 30)
                 else:
-                    print(f"{Fore.RED}Failed to decrypt file. File not found.{Style.RESET_ALL}")
+                    print(f"{Fore.RED}Failed to decrypt file. File not found or wrong password.{Style.RESET_ALL}")
 
 if __name__ == "__main__":
     main()
